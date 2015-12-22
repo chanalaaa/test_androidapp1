bf2615c5bc21cd4c0a8b67c23dc8c2eaf26048ba
Chanala Wilangka <yaicw49@hotmail.com>
2015-12-22 23:54:45 +0700

    extends BaseAdapter, Implement method

--- app/src/main/java/com/example/chanalaaa/app01/MyAdapter.java
+++ app/src/main/java/com/example/chanalaaa/app01/MyAdapter.java
@@ -1,12 +1,15 @@
 package com.example.chanalaaa.app01;
 
 import android.content.Context;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
 
 
 /**
  * Created by Chanalaaa on 22/12/2015.
  */
-public class MyAdapter {
+public class MyAdapter extends BaseAdapter {
 
     private Context objContext;
     private int[] trafficInts;
@@ -20,5 +23,24 @@ public class MyAdapter {
         //Constructor
     }
 
-    
+
+    @Override
+    public int getCount() {
+        return 0;
+    }
+
+    @Override
+    public Object getItem(int position) {
+        return null;
+    }
+
+    @Override
+    public long getItemId(int position) {
+        return 0;
+    }
+
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent) {
+        return null;
+    }
 }
